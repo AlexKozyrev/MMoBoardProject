5e765cd37ce808420390f4c845fa6f1b5a4d73b7
AlexKozyrev <JunkoEnoshimaEvil@yandex.ru>
2024-01-27 16:06:01 +0300

    Основа стабильная

--- project/board/urls.py
+++ project/board/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-# Импортируем созданное нами представление
-from .views import HomePageView, AdCreate, ResponseCreateView
+from .views import HomePageView, AdCreate, ResponseCreateView, AdDetailView, private_cabinet, AdUpdate, AdDelete
 
 urlpatterns = [
-   path('', HomePageView.as_view()),
+   path('', HomePageView.as_view(), name='home'),
+   path('ad/<int:pk>/', AdDetailView.as_view(), name='ad_detail'),
    path('response/<int:pk>/', ResponseCreateView.as_view(), name='response_create'),
+   path('pc/', private_cabinet, name='private_cabinet'),
    path('create/', AdCreate.as_view(), name='ad_create'),
+   path('<int:pk>/update/', AdUpdate.as_view(), name='ad_update'),
+   path('<int:pk>/delete/', AdDelete.as_view(), name='ad_delete'),
 ]
--- project/board/views.py
+++ project/board/views.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import PermissionRequiredMixin
-from django.shortcuts import render
-from django.urls import reverse
-from django.views.generic import ListView, CreateView
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import render, redirect
+from django.urls import reverse, reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 from .forms import AdForm
 from .models import Ad, Response
@@ -16,6 +18,12 @@ class HomePageView(ListView):
     def get_queryset(self):
         queryset = super().get_queryset()
         return queryset.order_by('-id')
+
+
+class AdDetailView(DetailView):
+    model = Ad
+    template_name = 'ad_detail.html'
+    context_object_name = 'ad'
 
 
 class ResponseCreateView(PermissionRequiredMixin, CreateView):
@@ -47,3 +55,66 @@ class AdCreate(PermissionRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+
+class AdUpdate(PermissionRequiredMixin, UpdateView):
+    permission_required = ('board.update_ad',)
+    form_class = AdForm
+    raise_exception = True
+    model = Ad
+    template_name = 'ad_edit.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.user != request.user:
+            raise PermissionDenied
+        return super().dispatch(request, *args, **kwargs)
+
+
+class AdDelete(PermissionRequiredMixin, DeleteView):
+    permission_required = ('board.delete_ad',)
+    raise_exception = True
+    model = Ad
+    template_name = 'ad_delete.html'
+    success_url = reverse_lazy('home')
+
+
+@login_required
+def private_cabinet(request):
+    user = request.user
+    ads = Ad.objects.filter(user=user)
+    responses = Response.objects.filter(ad__in=ads)
+
+    if request.method == "GET":
+        action = request.GET.get("action")
+        sort_by = request.GET.get("sort_by", "-dateCreation")
+
+        if action == "filter":
+            if sort_by == "content":
+                responses = responses.order_by("-dateCreation")
+            else:
+                responses = responses.order_by("-ad__dateCreation")
+
+    if request.method == "POST":
+        action = request.POST.get("action")
+
+        if action == "delete":
+            response_id = request.POST.get("response_id")
+            response = Response.objects.get(id=response_id)
+            if response.ad.user == user:
+                response.delete()
+                return redirect("private_cabinet")
+
+        if action == "accept":
+            response_id = request.POST.get("response_id")
+            response = Response.objects.get(id=response_id)
+            if response.ad.user == user:
+                response.accepted = True
+                response.save()
+                return redirect("private_cabinet")
+
+    return render(
+        request,
+        "private_cabinet.html",
+        {"responses": responses, "sort_by": sort_by},
+    )
